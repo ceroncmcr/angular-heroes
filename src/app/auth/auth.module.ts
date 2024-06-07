901131c9887aca47f8058e242207e9fbd85e1745
Cesar Ceron <ceron.cmcr@gmail.com>
2024-06-07 12:16:04 -0600

    Curso Angular - Seccion 14

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -12,8 +15,9 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
     RegisterPageComponent
   ],
   imports: [
-    CommonModule,
     AuthRoutingModule,
+    CommonModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  baseUrl: 'https://midominio.com/backend-heroes',
+};
+
